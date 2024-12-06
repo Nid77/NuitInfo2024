5ae448837f456e41c625078734ac20a765ca627f
rayan <rayan.tail@gonexa.fr>
2024-12-06 05:34:37 +0100

    update cryptage

--- src/app/badui/badui.component.ts
+++ src/app/badui/badui.component.ts
@@ -12,21 +12,46 @@ export class BaduiComponent {
   formGroup!: FormGroup;
   constructor(private fb: FormBuilder) {}
 
+  text: string = '';
   ngOnInit(): void {
     this.formGroup = this.fb.group({
-      inputValue: [''],
-    });
-    this.display();
-  }
-
-  private display(): void {
-    this.formGroup.get('inputValue')?.valueChanges.subscribe((value) => {
-      console.log('value : ' + value);
+      inputValue: [this.text],
     });
   }
 
-  encryptField() {
-    console.log('test');
+  onPaste(event: ClipboardEvent): void {
+    event.preventDefault();
+  }
+
+  public changeText(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    if (!value) {
+      return;
+    }
+    if (value.length > this.text.length) {
+      this.text += value[value.length - 1];
+    } else {
+      this.text = this.text.slice(0, value.length);
+    }
+    this.formGroup = this.fb.group({
+      inputValue: [this.cesarCipher(this.text, 12)],
+    });
+  }
+
+  cesarCipher(text: string, shift: number): string {
+    return text
+      .split('')
+      .map((char) => {
+        const charCode = char.charCodeAt(0);
+        if (char >= 'A' && char <= 'Z') {
+          return String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
+        }
+        if (char >= 'a' && char <= 'z') {
+          return String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
+        }
+        return char;
+      })
+      .join('');
   }
 
   //binary keyboard
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CaptchaComponent } from '../captcha/captcha.component';
+import { BaduiComponent } from '../badui/badui.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CaptchaComponent],
+  imports: [CaptchaComponent, BaduiComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
